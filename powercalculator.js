ac3462ef83dac06fed134ae194ed6d7ceee3b422
User <bo4arovpavel1989@mail.ru>
2018-08-01 13:04:29 +0300

    made some restyle

--- powercalculator.js
+++ powercalculator.js
@@ -8,13 +8,11 @@ class PowerCalculator {
 		this.dayTimeLeft = 14;
 		this.nightTimeLeft = 10;
 		this.maxPower;
-		this.rate;
 	}
 	
 	setData(data){
 		this.data = data;
 		this.maxPower = data.maxPower;
-		this.rate = data.rates[0];
 		
 		return this;
 	}
@@ -26,13 +24,12 @@ class PowerCalculator {
 			throw new Error('Input data must contain devices, rates and maxPower fields!')		
 	}
 	
-	markCheapestTime(val){ //помечаем период как используемый и возвращаем его
+	markCheapestTime(val){ //помечаем временной период как используемый и возвращаем его
 		let i;
 	
 		for (i = 0; i < this.data.rates.length; i++) {
 			if (this.data.rates[i].value === val && !this.data.rates[i].isMarked) {
 				this.data.rates[i].isMarked = true;
-				this.rate = this.data.rates[i];
 				break;
 			}	
 		}
@@ -40,7 +37,7 @@ class PowerCalculator {
 		return this.data.rates[i];
 	}
 	
-	getUnmarkedRates(){
+	getUnmarkedRates(){ //получаем массив временных периодов, которые еще не были имспользованы
 		let rates = [];
 		
 		this.data.rates.map(r => {
@@ -62,7 +59,12 @@ class PowerCalculator {
 		return this.markCheapestTime(val);
 	}
 	
-	getEmergencyNeededDevice(timeOfDay){ //выбираем устройство, которое в любом случае должно работать все оставшееся время
+	/*
+	выбираем устройство 1-й очереди, 
+	которое в любом случае должно работать все оставшееся время
+	*/
+	
+	getEmergencyNeededDevice(timeOfDay){ 
 		let devices = [];
 		
 		this.data.devices.map((d,i) => {
@@ -94,23 +96,21 @@ class PowerCalculator {
 		return devices;
 	}
 	
+	
 	/*
 	выбираем наиболее мощное устройство, 
 	но не превышающее остаточную мощность сети и еще не задействованное в 
 	текущем часе
-	*/
-	
-	
+	*/	
 	getMostExpensiveDevices(maxPower, timeOfDay, schedule){ 
 		let deviceID;
-		
-		let isMore = false;
+		let isMore;
 		let power = 0;		
 		
 		this.data.devices.map((d, i) => {
 			
-			if(!schedule.includes(d.id)) {
-				isMore = ((d.mode === undefined || d.mode === timeOfDay) && //день-ночь совпадает либо неважнол 
+			if(!schedule.includes(d.id)) { //устройства еще нет в расписании текущего часа
+				isMore = ((d.mode === undefined || d.mode === timeOfDay) && //режим день-ночь совпадает либо неважно
 				d.power > power && //мощность наибольшая из возможных
 				d.power <= maxPower && //мощность меньше остаточной
 				d.duration > 0) ? true : false; //устройство еще должно поработать		
@@ -140,32 +140,7 @@ class PowerCalculator {
 		this.checkData();
 		
 		let output = {
-			schedule: {
-				"0": [],
-				"1": [],
-				"2": [],
-				"3": [],
-				"4": [],
-				"5": [],
-				"6": [],
-				"7": [],
-				"8": [],
-				"9": [],
-				"10": [],
-				"11": [],
-				"12": [],
-				"13": [],
-				"14": [],
-				"15": [],
-				"16": [],
-				"17": [],
-				"18": [],
-				"19": [],
-				"20": [],
-				"21": [],
-				"22": [],
-				"23": []
-				},
+			schedule: {},
 			consumedEnergy: {value:0, devices: {}}
 		}
 		
@@ -173,17 +148,16 @@ class PowerCalculator {
 		while (this.timeLeft > 0) {
 			let currentRate = this.getCheapestTime();
 			let h = currentRate.from;
-			
-			//console.log(this.data)
-			
+						
 			while ( h !== currentRate.to) {
-				console.log(h);
 				let maxPower = this.maxPower;
-				let timeOfDay = (h >= 7 && h < 21) ? 'day' : 'night';
-								
+				let timeOfDay = (h >= 7 && h < 21) ? 'day' : 'night';		
 				let emergDevices = this.getEmergencyNeededDevice(timeOfDay);
 				
 				emergDevices.map(d => { //включаем в расписание устройства, которым в первую очередь необходимо работать
+					
+					if(!output.schedule[h.toString()])
+						output.schedule[h.toString()] = [];
 					
 					output.schedule[h.toString()].push(d.id);
 					
@@ -199,8 +173,13 @@ class PowerCalculator {
 					else 
 						output.consumedEnergy.devices[d.id] += d.power * currentRate.value;
 				});
-								
-				while (maxPower > 0) {	//на оставшуюся мощность выбираем наиболее мощные устройства				
+				
+				/*
+				на оставшуюся мощность выбираем наиболее мощные устройства,
+				чтобы они успели поработать в наиболее дешовое время
+				*/
+				
+				while (maxPower > 0) {				
 					let maxPowerDeviceId = this.getMostExpensiveDevices(maxPower, timeOfDay, output.schedule[h.toString()]);
 										
 					if (maxPowerDeviceId){
@@ -209,6 +188,9 @@ class PowerCalculator {
 						maxPower -= maxPowerDevice.power;
 						
 						maxPowerDevice.duration--;
+						
+						if(!output.schedule[h.toString()])
+							output.schedule[h.toString()] = [];
 						
 						output.schedule[h.toString()].push(maxPowerDeviceId);
 						
@@ -219,9 +201,13 @@ class PowerCalculator {
 						else 
 							output.consumedEnergy.devices[maxPowerDeviceId] += maxPowerDevice.power * currentRate.value;
 					} else {
-						maxPower = 0; //больше устройств не влезет в этот час
+						maxPower = 0; //больше устройств не влезет в этот час, обнуляем переменную чтобы закончить цикл
 					}
 				}
+				
+				/*
+				совершаем все временные сдвиги
+				*/
 				
 				this.timeLeft--;
 				
